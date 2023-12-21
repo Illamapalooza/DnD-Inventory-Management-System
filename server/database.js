0bfb0dfee4e134b8a3104f4abf6dcd3bcb34db5e
Illamapalooza <juliusbaliling25@gmail.com>
2023-12-21 22:53:51 +0800

    synchronize and connect to database with sequelize ORM

--- server/database.js
+++ server/database.js
@@ -1,39 +1,78 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import mysql from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
 
+/**
+ * The user credentials to connect the database
+ */
 const credentials = {
+ database: 'foodservicedb',
  username: 'root',
  password: 'root',
- database: 'foodservicedb',
 };
 
-const config = {
+/**
+ * The connection options of the model to the database
+ */
+const options = {
  host: 'localhost',
+ dialect: 'mysql',
+ define: {
+  // disable auto-generated timestamp columns
+  timestamps: false,
+  // make table names same as model
+  freezeTableName: true,
+ },
+ // Create a pool of connections for improved performance
  pool: {
-  max: 5,
-  min: 0,
-  idle: 10000,
+  max: 10, // Maximum number of connections in pool
+  min: 0, // Minimum number of connections in pool
+  acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
+  idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released
  },
 };
 
-const connection = new Sequelize({
- username: credentials.username,
- password: credentials.password,
- database: credentials.database,
- dialect: 'mysql',
- config,
-});
+/**
+ * The session object to the database
+ * @type {Sequelize}
+ */
+const session = new Sequelize(
+ credentials.database,
+ credentials.username,
+ credentials.password,
+ options
+);
 
-try {
- await sequelize.authenticate();
- console.log('Connection has been established successfully.');
-} catch (error) {
- console.error('Unable to connect to the database:', error);
-}
+/**
+ * Synchronize the model with the database
+ * @param {Object} options
+ */
+const synchronize = async function (options) {
+ await session.sync(options);
+ await session.close();
+};
 
-// // Syncing models with the database
-// sequelize.sync({ force: false }).then(() => {
-//  console.log('Database & tables created with associations!');
-// });
+/**
+ * Check the database connection
+ * returns {boolean} connection status of the database
+ */
+const connection = async () => {
+ const connection = new Sequelize(
+  '',
+  credentials.username,
+  credentials.password,
+  options
+ );
+ await connection
+  .authenticate()
+  .then(() => {
+   console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+   console.error('Unable to connect to the database:', err);
+  });
 
-export default connection;
+ return connection;
+};
+
+const Database = { session, options, credentials, synchronize, connection };
+
+export default Database;
