d9f0c079daaec1172477d38c2205895e55e1f762
Illamapalooza <juliusbaliling25@gmail.com>
2023-12-25 14:08:16 +0800

    added tailwind navbar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,11 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Fragment, useState } from 'react';
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
  { name: 'Home', href: '#', current: true },
  { name: 'Menu', href: '#', current: false },
- { name: 'Order', href: '#', current: false },
+ { name: 'Booking', href: '#', current: false },
  { name: 'Contact', href: '#', current: false },
 ];
 
@@ -13,154 +13,105 @@ function classNames(...classes) {
  return classes.filter(Boolean).join(' ');
 }
 
-export default function Navbar() {
- return (
-  <Disclosure as="nav" className="bg-transparent fixed top-0 w-full">
-   {({ open }) => (
-    <>
-     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div className="relative flex h-24 items-center justify-between">
-       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        {/* Mobile menu button*/}
-        <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-linen-800 hover:bg-bistre hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-         <span className="absolute -inset-0.5" />
-         <span className="sr-only">Open main menu</span>
-         {open ? (
-          <XMarkIcon className="block h-8 w-8" aria-hidden="true" />
-         ) : (
-          <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
-         )}
-        </Disclosure.Button>
-       </div>
-       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
-         <img
-          className="h-12 w-auto"
-          src="https://seeklogo.com/images/S/starbucks-coffee-logo-0639383401-seeklogo.com.png"
-          alt="Your Company"
-         />
-        </div>
-        <div className="hidden sm:ml-6 sm:block">
-         <div className="flex space-x-4">
-          {navigation.map((item) => (
-           <a
-            key={item.name}
-            href={item.href}
-            className={classNames(
-             item.current
-              ? 'bg-linen-700 text-bistre'
-              : 'text-linen-700 ease-in-out duration-300 hover:bg-bistre hover:text-linen-700',
-             'rounded-md px-3 py-2 text-2xl font-medium'
-            )}
-            aria-current={item.current ? 'page' : undefined}
-           >
-            {item.name}
-           </a>
-          ))}
-         </div>
-        </div>
-       </div>
-       <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button
-         type="button"
-         className="relative rounded-full bg-linen p-1 text-bistre hover:text-licorine focus:outline-none focus:ring-2 focus:ring-bistre focus:ring-offset-2 focus:ring-offset-linen"
-        >
-         <span className="absolute -inset-1.5" />
-         <span className="sr-only">View notifications</span>
-         <BellIcon className="h-8 w-8" aria-hidden="true" />
-        </button>
+export default function NavBar() {
+ const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-        {/* Profile dropdown */}
-        <Menu as="div" className="relative ml-3">
-         <div>
-          <Menu.Button className="relative flex rounded-full bg-linen p-1 text-bistre hover:text-licorine focus:outline-none focus:ring-2 focus:ring-linen focus:ring-offset-2 focus:ring-offset-linen">
-           <span className="absolute -inset-1.5" />
-           <span className="sr-only">Open user menu</span>
-           <img
-            className="h-10 w-10 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-           />
-          </Menu.Button>
-         </div>
-         <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+ return (
+  <header className="bg-transparent top-0 fixed w-full">
+   <nav
+    className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 md:px-6"
+    aria-label="Global"
+   >
+    <div className="sm:hidden"></div>
+    <div className="flex sm:flex-1">
+     <a href="#" className="-m-1.5 p-1.5">
+      <span className="sr-only">Your Company</span>
+      <img
+       className="h-10 w-auto"
+       src="https://seeklogo.com/images/S/starbucks-coffee-logo-0639383401-seeklogo.com.png"
+       alt="Codex Coffee"
+      />
+     </a>
+    </div>
+    <div className="flex sm:hidden">
+     <button
+      type="button"
+      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-linen-700"
+      onClick={() => setMobileMenuOpen(true)}
+     >
+      <span className="sr-only">Open main menu</span>
+      <Bars3Icon className="h-8 w-8" aria-hidden="true" />
+     </button>
+    </div>
+    <div className="hidden sm:flex sm:gap-x-12">
+     {navigation.map((item) => (
+      <a
+       key={item.name}
+       href={item.href}
+       className={classNames(
+        item.current
+         ? 'text-large font-medium leading-6 text-linen overline overline-offset-[0.4rem] decoration-2'
+         : 'text-large font-medium leading-6 text-linen hover:overline overline-offset-[0.4rem] decoration-2'
+       )}
+       aria-current={item.current ? 'page' : undefined}
+      >
+       {item.name}
+      </a>
+     ))}
+    </div>
+    <div className="hidden sm:flex sm:flex-1 sm:justify-end">
+     <a href="#" className="text-large font-medium leading-6 text-linen-900">
+      Log in <span aria-hidden="true">&rarr;</span>
+     </a>
+    </div>
+   </nav>
+
+   {/* small size screen*/}
+   <Dialog
+    as="div"
+    className="sm:hidden"
+    open={mobileMenuOpen}
+    onClose={setMobileMenuOpen}
+   >
+    <div className="fixed inset-0 z-10" />
+    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-linen px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-linen">
+     <div className="flex justify-between">
+      <div></div>
+      <a href="#" className="-m-1.5 p-1.5 place-self-center">
+       <span className="sr-only">Your Company</span>
+       <img
+        className="h-10 w-auto "
+        src="https://seeklogo.com/images/S/starbucks-coffee-logo-0639383401-seeklogo.com.png"
+        alt=""
+       />
+      </a>
+      <button
+       type="button"
+       className="-m-2.5 rounded-md p-2.5 text-bistre-700 justify-self-end"
+       onClick={() => setMobileMenuOpen(false)}
+      >
+       <span className="sr-only">Close menu</span>
+       <XMarkIcon className="h-8 w-8" aria-hidden="true" />
+      </button>
+     </div>
+     <div className="mt-6 flow-root">
+      <div className="-my-6 divide-y divide-gray-500/10">
+       <div className="space-y-2 py-6">
+        {navigation.map((item) => (
+         <a
+          key={item.name}
+          href={item.href}
+          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-bistre hover:bg-bone1"
+          aria-current={item.current ? 'page' : undefined}
          >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-linen py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-           <Menu.Item>
-            {({ active }) => (
-             <a
-              href="#"
-              className={classNames(
-               active ? 'bg-bone2-400' : '',
-               'block px-4 py-2 text-sm text-bistre'
-              )}
-             >
-              Your Profile
-             </a>
-            )}
-           </Menu.Item>
-           <Menu.Item>
-            {({ active }) => (
-             <a
-              href="#"
-              className={classNames(
-               active ? 'bg-bone2-400' : '',
-               'block px-4 py-2 text-sm text-bistre'
-              )}
-             >
-              Settings
-             </a>
-            )}
-           </Menu.Item>
-           <Menu.Item>
-            {({ active }) => (
-             <a
-              href="#"
-              className={classNames(
-               active ? 'bg-bone2-400' : '',
-               'block px-4 py-2 text-sm text-bistre'
-              )}
-             >
-              Sign out
-             </a>
-            )}
-           </Menu.Item>
-          </Menu.Items>
-         </Transition>
-        </Menu>
+          {item.name}
+         </a>
+        ))}
        </div>
       </div>
      </div>
-
-     <Disclosure.Panel className="sm:hidden">
-      <div className="space-y-1 px-2 pb-3 pt-2">
-       {navigation.map((item) => (
-        <Disclosure.Button
-         key={item.name}
-         as="a"
-         href={item.href}
-         className={classNames(
-          item.current
-           ? 'bg-bistre text-linen-800'
-           : 'text-linen-700 hover:bg-bistre-600 hover:text-linen-800',
-          'block rounded-md px-3 py-2 text-base font-medium'
-         )}
-         aria-current={item.current ? 'page' : undefined}
-        >
-         {item.name}
-        </Disclosure.Button>
-       ))}
-      </div>
-     </Disclosure.Panel>
-    </>
-   )}
-  </Disclosure>
+    </Dialog.Panel>
+   </Dialog>
+  </header>
  );
 }
