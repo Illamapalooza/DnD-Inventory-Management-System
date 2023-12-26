300de28dad75a9717a9a71bd636da198f5315bfb
Illamapalooza <juliusbaliling25@gmail.com>
2023-12-26 14:18:58 +0800

    render menu contact and hero sections

--- client/src/routes/Landing.jsx
+++ client/src/routes/Landing.jsx
@@ -1,11 +1,14 @@
 import Navbar from '../components/NavBar.jsx';
-import Banner from '../components/Banner.jsx';
 import Hero from '../components/Hero.jsx';
+import Menu from '../components/Menu.jsx';
+import Contact from '../components/Contact.jsx';
 
 const Home = () => {
  return (
   <div className="bg-bone2">
    <Hero />
+   <Menu />
+   <Contact />
    <Navbar />
   </div>
  );
