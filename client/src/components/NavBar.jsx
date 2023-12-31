729b4c1e219748bfb20803a28d1193ef696eb292
Illamapalooza <juliusbaliling25@gmail.com>
2023-12-31 19:53:15 +0800

    added navbar feature

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,138 +1,53 @@
-import { Fragment, useState, useEffect } from 'react';
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import logo from '../assets/logo.png';
+import React from 'react';
 
-const navigation = [
- { id: 'home', name: 'Home', href: '#', current: false },
- { id: 'menu', name: 'Menu', href: '#menu', current: false },
- { id: 'booking', name: 'Booking', href: '#', current: false },
- { id: 'contact', name: 'Contact', href: '#', current: false },
-];
-
-function classNames(...classes) {
- return classes.filter(Boolean).join(' ');
-}
-
-export default function NavBar() {
- const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
- const [hasScrolled, setHasScrolled] = useState(false);
- const [current, setCurrent] = useState('home');
-
- useEffect(() => {
-  const handleScroll = () => {
-   const viewportHeight = window.innerHeight;
-   const scrollPosition = window.scrollY;
-
-   if (scrollPosition >= viewportHeight - 100) {
-    setHasScrolled(true);
-   } else {
-    setHasScrolled(false);
-   }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-   window.removeEventListener('scroll', handleScroll);
-  };
- }, []);
-
+const Header = () => {
  return (
-  <header
-   className={
-    hasScrolled
-     ? 'bg-bistre-200 top-0 fixed w-full bg-opacity-80'
-     : 'bg-transparent top-0 fixed w-full'
-   }
-  >
-   <nav
-    className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8 md:px-6"
-    aria-label="Global"
-   >
-    <div className="sm:hidden"></div>
-    <div className="flex sm:flex-1">
-     <a href="#" className="-m-1.5 p-1.5">
-      <span className="sr-only">Your Company</span>
-      <img className="h-10 w-auto" src={logo} alt="Codex Coffee" />
-     </a>
-    </div>
-    <div className="flex sm:hidden">
-     <button
-      type="button"
-      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-linen-700"
-      onClick={() => setMobileMenuOpen(true)}
-     >
-      <span className="sr-only">Open main menu</span>
-      <Bars3Icon className="h-8 w-8" aria-hidden="true" />
-     </button>
-    </div>
-    <div className="hidden sm:flex sm:gap-x-12">
-     {navigation.map((item) => (
-      <a
-       key={item.name}
-       href={item.href}
-       className={classNames(
-        current === item.id
-         ? 'text-large font-medium leading-6 text-linen overline overline-offset-[0.4rem] decoration-2'
-         : 'text-large font-medium leading-6 text-linen hover:overline overline-offset-[0.4rem] decoration-2'
-       )}
-       onClick={() => setCurrent(item.id)}
-       aria-current={item.current ? 'page' : undefined}
-      >
-       {item.name}
-      </a>
-     ))}
-    </div>
-    <div className="hidden sm:flex sm:flex-1 sm:justify-end">
-     <a href="#" className="text-large font-medium leading-6 text-linen-900">
-      Log in <span aria-hidden="true">&rarr;</span>
-     </a>
-    </div>
-   </nav>
+  <nav className="fixed top-0 z-50 bg-white shadow-md p-4 flex justify-between items-center w-full">
+   <div className="logo">
+    <img src="/path-to-your-logo.png" alt="Brand Logo" className="h-8" />
+   </div>
 
-   {/* small size screen*/}
-   <Dialog
-    as="div"
-    className="sm:hidden"
-    open={mobileMenuOpen}
-    onClose={setMobileMenuOpen}
-   >
-    <div className="fixed inset-0 z-10" />
-    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-linen px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-linen">
-     <div className="flex justify-between">
-      <div></div>
-      <a href="#" className="-m-1.5 p-1.5 place-self-center">
-       <span className="sr-only">Your Company</span>
-       <img className="h-10 w-auto " src={logo} alt="" />
-      </a>
-      <button
-       type="button"
-       className="-m-2.5 rounded-md p-2.5 text-bistre-700 justify-self-end"
-       onClick={() => setMobileMenuOpen(false)}
-      >
-       <span className="sr-only">Close menu</span>
-       <XMarkIcon className="h-8 w-8" aria-hidden="true" />
-      </button>
-     </div>
-     <div className="mt-6 flow-root">
-      <div className="-my-6 divide-y divide-gray-500/10">
-       <div className="space-y-2 py-6">
-        {navigation.map((item) => (
-         <a
-          key={item.name}
-          href={item.href}
-          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-bistre hover:bg-bone1"
-          aria-current={item.current ? 'page' : undefined}
-         >
-          {item.name}
-         </a>
-        ))}
-       </div>
-      </div>
-     </div>
-    </Dialog.Panel>
-   </Dialog>
-  </header>
+   <div className="icons flex items-center space-x-4">
+    {/* Notifications Icon */}
+    <svg
+     xmlns="http://www.w3.org/2000/svg"
+     fill="none"
+     viewBox="0 0 24 24"
+     strokeWidth={1.5}
+     stroke="currentColor"
+     className="h-6 w-6 text-gray-600 cursor-pointer"
+    >
+     <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+     />
+    </svg>
+
+    {/* User Avatar */}
+    {/* <img
+     src="/path-to-your-avatar.jpg"
+     alt="User Avatar"
+     className="h-8 w-8 rounded-full"
+    /> */}
+
+    <svg
+     xmlns="http://www.w3.org/2000/svg"
+     fill="none"
+     viewBox="0 0 24 24"
+     strokeWidth={1.5}
+     stroke="currentColor"
+     className="w-6 h-6"
+    >
+     <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+     />
+    </svg>
+   </div>
+  </nav>
  );
-}
+};
+
+export default Header;
