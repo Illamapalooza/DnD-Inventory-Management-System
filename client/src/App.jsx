50812dc0dedf8c85aa027d33b30a9e651cc1c08e
Illamapalooza <juliusbaliling25@gmail.com>
2023-12-31 19:47:21 +0800

    added react routes for login, registration, dashboard, products, and addproducts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './routes/Landing.jsx';
+import Login from './routes/Login.jsx';
+import Registration from './routes/Register.jsx';
+import Dashboard from './routes/Dashboard.jsx';
+import Products from './routes/Products.jsx';
+import AddProducts from './routes/AddProducts.jsx';
 
 const App = () => {
  return (
   <div className="App">
    <Router>
     <Routes>
-     <Route path="/" exact element={Landing()} />
+     {/* <Route path="/" exact element={Login()} /> */}
+     <Route path="/register" exact element={Registration()}></Route>
+     <Route path="/dashboard" exact element={Dashboard()}></Route>
+     <Route path="/products" exact element={Products()}></Route>
+     <Route path="/add-products" exact element={AddProducts()}></Route>
     </Routes>
    </Router>
   </div>
