cc39f69a1cd3836202201fdc01c58fb53edb68ab
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-01 23:02:20 +0800

    added hyperlink to add product button

--- client/src/routes/Products.jsx
+++ client/src/routes/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Sidebar from '../components/Sidebar';
 import ProductList from '../components/ProductList';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
  return (
@@ -12,26 +13,28 @@ const Products = () => {
     <div className="p-4 mt-10">
      <div className="flex flex-row w-full justify-between items-center">
       <h1 className="text-2xl font-bold text-gray-80 py-6">Products</h1>
-      <button
-       type="button"
-       className="text-white bg-kelly-500 hover:bg-kelly-400 focus:ring-4 focus:outline-none focus:ring-kelly-700 font-medium rounded-lg text-md px-4 py-2.5 text-center inline-flex items-center justify-between"
-      >
-       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-5 h-5 mr-1"
+      <Link to="/add-products">
+       <button
+        type="button"
+        className="text-white bg-kelly-500 hover:bg-kelly-400 focus:ring-4 focus:outline-none focus:ring-kelly-700 font-medium rounded-lg text-md px-4 py-2.5 text-center inline-flex items-center justify-between"
        >
-        <path
-         strokeLinecap="round"
-         strokeLinejoin="round"
-         d="M12 4.5v15m7.5-7.5h-15"
-        />
-       </svg>
-       <div>Add Product</div>
-      </button>
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         fill="none"
+         viewBox="0 0 24 24"
+         strokeWidth={1.5}
+         stroke="currentColor"
+         className="w-5 h-5 mr-1"
+        >
+         <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+         />
+        </svg>
+        <div>Add Product</div>
+       </button>
+      </Link>
      </div>
 
      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
