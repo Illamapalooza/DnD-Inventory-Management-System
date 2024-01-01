8862853e984554a738838195810bcf3468828102
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-01 11:37:51 +0800

    added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,23 @@ import Registration from './routes/Register.jsx';
 import Dashboard from './routes/Dashboard.jsx';
 import Products from './routes/Products.jsx';
 import AddProducts from './routes/AddProducts.jsx';
+import Orders from './routes/Orders.jsx';
+import AddOrders from './routes/AddOrders.jsx';
+import Suppliers from './routes/Suppliers.jsx';
 
 const App = () => {
  return (
   <div className="App">
    <Router>
     <Routes>
-     {/* <Route path="/" exact element={Login()} /> */}
+     <Route path="/login" exact element={Login()} />
      <Route path="/register" exact element={Registration()}></Route>
      <Route path="/dashboard" exact element={Dashboard()}></Route>
      <Route path="/products" exact element={Products()}></Route>
      <Route path="/add-products" exact element={AddProducts()}></Route>
+     <Route path="/orders" exact element={Orders()}></Route>
+     <Route path="/add-orders" exact element={AddOrders()}></Route>
+     <Route path="/suppliers" exact element={Suppliers()}></Route>
     </Routes>
    </Router>
   </div>
