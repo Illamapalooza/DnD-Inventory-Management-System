1edbae524fcd1e2025feb9aa5894868b46821f40
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-01 19:58:18 +0800

    added backend functionality for login

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import logo from '../assets/dnd-svg.svg';
-
-const handleSubmit = (e) => {
- e.preventDefault();
-};
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Validation from './LoginValidation.jsx';
+import axios from 'axios';
 
 const LoginForm = () => {
+ const [formData, setFormData] = useState({
+  email: '',
+  password: '',
+ });
+
+ const [errors, setErrors] = useState({});
+
+ const handleInput = (e) => {
+  setFormData((prevFormData) => ({
+   ...prevFormData,
+   [e.target.name]: e.target.value,
+  }));
+ };
+
+ const navigate = useNavigate();
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  setErrors(Validation(formData));
+  if (errors.email === '' && errors.password === '') {
+   axios
+    .post('http://localhost:3000/login', formData)
+    .then((res) => {
+     if (res.status === 200) {
+      //   localStorage.setItem('token', res.data.token);
+      navigate('/dashboard');
+     } else {
+      alert('Invalid Credentials');
+     }
+    })
+    .catch((err) => {
+     console.log(err);
+    });
+  }
+ };
+
  return (
   <div className="flex h-screen justify-center items-center">
    <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md">
@@ -20,14 +56,20 @@ const LoginForm = () => {
 
      <p className="mt-1 text-center text-grayish">Login</p>
 
-     <form onSubmit={handleSubmit}>
+     <form onSubmit={handleSubmit} action="">
       <div className="w-full mt-4">
        <input
         className="block w-full px-4 py-2 mt-2 text-foreground placeholder-gray-500 bg-white border rounded-lg focus:border-kelly-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-kelly-300"
-        type="email"
         placeholder="Email Address"
         aria-label="Email Address"
+        name="email"
+        onChange={handleInput}
        />
+       {errors.email && (
+        <span className="font-thin text-red-600 text-[12px] ml-2">
+         {errors.email}
+        </span>
+       )}
       </div>
 
       <div className="w-full mt-4">
@@ -36,7 +78,14 @@ const LoginForm = () => {
         type="password"
         placeholder="Password"
         aria-label="Password"
+        name="password"
+        onChange={handleInput}
        />
+       {errors.password && (
+        <span className="font-thin text-red-600 text-[12px] ml-2">
+         {errors.password}
+        </span>
+       )}
       </div>
 
       <div className="flex items-center justify-between mt-4">
@@ -44,7 +93,10 @@ const LoginForm = () => {
         Forget Password?
        </a>
 
-       <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-kelly-500 rounded-lg hover:bg-kelly-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+       <button
+        type="submit"
+        className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-kelly-500 rounded-lg hover:bg-kelly-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+       >
         Sign In
        </button>
       </div>
@@ -54,12 +106,12 @@ const LoginForm = () => {
     <div className="flex items-center justify-center py-4 text-center bg-white">
      <span className="text-sm text-gray-600">Don't have an account?</span>
 
-     <a
-      href="#"
+     <Link
+      to="/register"
       className="mx-2 text-sm font-bold text-kelly-500 hover:underline"
      >
       Register
-     </a>
+     </Link>
     </div>
    </div>
   </div>
