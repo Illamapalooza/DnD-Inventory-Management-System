5e61be0f1057b06090e3a41a152238f554207fb9
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-01 20:03:15 +0800

    added register and login requests

--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -4,14 +4,37 @@ import Users from '../models/Users.js';
 
 const UsersRouter = Router();
 
-UsersRouter.get('/', async (req, res) => {
- const users = await Users.findAll();
+UsersRouter.post('/login', async (req, res) => {
+ const users = await Users.findOne({
+  where: { Email: req.body.email, Password: req.body.password },
+ })
+  .then((user) => {
+   if (!user) {
+    res.status(404).send('User not found');
+   } else {
+    if (user.Password === req.body.password) {
+     res.status(200).json(user);
+    } else {
+     res.status(401).send('Incorrect password');
+    }
+   }
+  })
+  .catch((err) => {
+   console.log('Error: ', err);
+  });
  res.json(users);
 });
 
-UsersRouter.post('/', async (req, res) => {
+UsersRouter.post('/register', async (req, res) => {
  const post = req.body;
- await Users.create(post);
+ await Users.create({
+  Name: post.name,
+  Password: post.password,
+  Address: post.address,
+  Phone: post.phone,
+  Email: post.email,
+  Role: post.role,
+ });
  res.json(post);
 });
 
