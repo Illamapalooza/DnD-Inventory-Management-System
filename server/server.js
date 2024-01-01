589091820b21358e7dda49123f94f8494d40dd7b
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-01 20:01:50 +0800

    added cors functionalty

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import OrdersRouter from './routes/OrdersRouter.js';
 import UsersRouter from './routes/UsersRouter.js';
+import Users from './models/Users.js';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/orders', OrdersRouter);
-app.use('/users', UsersRouter);
+app.use('/', UsersRouter);
 
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}.`);
