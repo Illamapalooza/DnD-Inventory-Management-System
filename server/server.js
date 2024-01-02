311b89ef34288de16e0bf88e77138d774099dd9b
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-02 18:31:24 +0800

    added SupplierRouters middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import OrdersRouter from './routes/OrdersRouter.js';
 import UsersRouter from './routes/UsersRouter.js';
 import ProductsRouter from './routes/ProductsRouter.js';
 import cors from 'cors';
+import SuppliersRouter from './routes/SuppliersRouter.js';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/orders', OrdersRouter);
 app.use('/', UsersRouter);
 app.use('/products', ProductsRouter);
+app.use('/suppliers', SuppliersRouter);
 
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}.`);
