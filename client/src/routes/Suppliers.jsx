76b94628347a2fabc6eac6e4644427032643d39a
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-02 18:30:41 +0800

    added link to AddSupplier route for add suppliers button

--- client/src/routes/Suppliers.jsx
+++ client/src/routes/Suppliers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import NavBar from '../components/NavBar';
 import SupplierList from '../components/SupplierList';
+import { Link } from 'react-router-dom';
 
 const Suppliers = () => {
  return (
@@ -12,30 +13,32 @@ const Suppliers = () => {
     <div className="w-full p-4 mt-10">
      <div className="flex flex-row w-full justify-between items-center">
       <h1 className="text-2xl font-bold text-gray-80 py-6">Suppliers</h1>
-      <button
-       type="button"
-       className="text-white bg-kelly-500 hover:bg-kelly-400 focus:ring-4 focus:outline-none focus:ring-kelly-700 font-medium rounded-lg text-md px-4 py-2.5 text-center inline-flex items-center justify-between"
-      >
-       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-5 h-5 mr-1"
+      <Link to="/suppliers/add-suppliers">
+       {' '}
+       <button
+        type="button"
+        className="text-white bg-kelly-500 hover:bg-kelly-400 focus:ring-4 focus:outline-none focus:ring-kelly-700 font-medium rounded-lg text-md px-4 py-2.5 text-center inline-flex items-center justify-between"
        >
-        <path
-         strokeLinecap="round"
-         strokeLinejoin="round"
-         d="M12 4.5v15m7.5-7.5h-15"
-        />
-       </svg>
-       <div>Add Supplier</div>
-      </button>
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         fill="none"
+         viewBox="0 0 24 24"
+         strokeWidth={1.5}
+         stroke="currentColor"
+         className="w-5 h-5 mr-1"
+        >
+         <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+         />
+        </svg>
+        <div>Add Supplier</div>
+       </button>
+      </Link>
      </div>
     </div>
     <div>
-     {' '}
      <SupplierList />
     </div>
    </div>
