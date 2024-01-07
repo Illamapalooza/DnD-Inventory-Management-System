b4a8f96e2c00866beb696d3712b00b1fd69e609c
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-07 19:58:54 +0800

    added Deliveries link

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -154,11 +154,11 @@ const Sidebar = () => {
 
        <span className="mx-2 text-sm font-medium">Orders</span>
       </Link>
-
-      <a
+      <Link
+       to="/deliveries"
        className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg hover:bg-gray-100   hover:text-gray-700"
-       href="#"
       >
+       {' '}
        <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -173,9 +173,8 @@ const Sidebar = () => {
          d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
         />
        </svg>
-
        <span className="mx-2 text-sm font-medium">Deliveries</span>
-      </a>
+      </Link>
      </div>
 
      <div className="space-y-3 ">
