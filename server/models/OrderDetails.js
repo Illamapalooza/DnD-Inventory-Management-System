b5f285309920f897330772ee65c3c19c065f7b79
Illamapalooza <juliusbaliling25@gmail.com>
2024-01-08 15:19:38 +0800

    added aftercreate function for automating inventory item creation in database

--- server/models/OrderDetails.js
+++ server/models/OrderDetails.js
@@ -1,5 +1,6 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, QueryTypes, where } from 'sequelize';
 import Database from '../database.js';
+import Inventory from './Inventory.js';
 
 const fields = {
  OrderDetailID: {
@@ -28,5 +29,34 @@ const fields = {
 };
 
 const OrderDetails = Database.session.define('OrderDetails', fields);
+
+OrderDetails.afterCreate(async (orderDetail) => {
+ const inventoryItems = await Database.session.query(
+  `SELECT Quantity FROM Inventory WHERE ProductID = ${orderDetail.ProductID}`,
+  { type: QueryTypes.SELECT }
+ );
+
+ const orderStatus = await Database.session.query(
+  `SELECT Status FROM Orders WHERE OrderID = ${orderDetail.OrderID}`,
+  { type: QueryTypes.SELECT }
+ );
+
+ console.log('Order Status is', orderStatus[0].Status);
+
+ console.log('Inventory Stock is', orderDetail.Quantity);
+
+ if (inventoryItems.length > 0 && orderStatus[0].Status === 'Delivered') {
+  const inventoryItemQuantity = inventoryItems[0].Quantity; // Accessing the quantity of the first (and likely only) item
+
+  console.log('Inventory Stock is', inventoryItemQuantity);
+  // Update the quantity and last order date
+  const newQuantity =
+   parseInt(inventoryItemQuantity) + parseInt(orderDetail.Quantity);
+  await Database.session.query(
+   `UPDATE Inventory SET Quantity = ${newQuantity}, LastOrderDate = NOW() WHERE ProductID = ${orderDetail.ProductID}`,
+   { type: QueryTypes.UPDATE }
+  );
+ }
+});
 
 export default OrderDetails;
